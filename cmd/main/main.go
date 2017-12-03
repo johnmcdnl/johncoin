8de8eea36f6fe406f74c0a085dedd1f180c2b97b
John McDonnell <john.mcdonnell439@gmail.com>
2017-12-03 22:13:07 +0000

    adding proof of work

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/johnmcdnl/johncoin"
+	"strconv"
 )
 
 func main() {
 	bc := johncoin.NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	bc.AddBlock("Send 1 JC to Ivan")
+	bc.AddBlock("Send 2 more JC to Ivan")
+	bc.AddBlock("Send 1 JC to John")
+	bc.AddBlock("Send 10 JC to Jim")
+	bc.AddBlock("Send 4 more JC to Jim")
 
 	for _, block := range bc.Blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := johncoin.NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
+	fmt.Println()
 }
+
